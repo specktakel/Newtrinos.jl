ca06b0e562c0a9acc42eafc7f4a9054eb95cb48b
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2026-07-29 10:08:59 +0200

    add daya bay flux

--- src/physics/reactor_flux.jl
+++ src/physics/reactor_flux.jl
@@ -19,18 +19,17 @@ abstract type FluxSystematicsModel end
 
 
 @kwdef struct ReactorFluxConfig{F<:NominalFluxModel, S<:FluxSystematicsModel}
-    nominal_model::F = DayaBayFlux()
-    systematics_model::S = DayaBaySystematics()
-    isotope_ratio::NamedTuple = (U_235 = 1., U_239 = 0., Pu_241 = 0.,)
+    isotope_ratio::NamedTuple = (U_235 = 0.61, Pu_239 = 0.33, Pu_241 = 0.06,)
+    nominal_model::F = HuberFlux(isotope_ratio)
+    systematics_model::S = HuberSystematics()
 end
 
 
 struct HuberFlux <: NominalFluxModel
-    cfg::ReactorFluxConfig
+    isotope_ratio::NamedTuple
 end
 
 struct HuberSystematics <: FluxSystematicsModel
-    cfg::ReactorFluxConfig
 end
 
 
@@ -52,7 +51,7 @@ end
     sys_flux::Function
 end
 
-function configure(cfg::ReactorFluxConfig=ReactorFluxConfig())
+function configure(cfg::ReactorFluxConfig = ReactorFluxConfig())
     ReactorFlux(
         cfg=cfg,
         params = get_params(cfg.nominal_model),
@@ -72,7 +71,7 @@ function get_priors(cfg::HuberFlux)
 end
 
 function get_nominal_flux(cfg::HuberFlux)
-    isotope_ratio = cfg.cfg.isotope_ratio
+    isotope_ratio = cfg.isotope_ratio
     U_235 = [4.367, -4.577, 2.100, -5.294e-1, 6.186e-2, -2.777e-3]
     Pu_239 = [4.757, -5.392, 2.563, -6.596e-1, 7.820e-2, -3.536e-3]
     Pu_241 = [2.990, -2.882, 1.278, -3.343e-1, 3.905e-2, -1.754e-3]
@@ -93,6 +92,10 @@ function get_nominal_flux(cfg::HuberFlux)
     end
 
     nominal_flux
+end
+
+function get_sys_flux(cfg::HuberFlux)
+    func(x) = 0.
 end
 
 function get_priors(cfg::HuberFlux)
@@ -116,26 +119,77 @@ end
 """
 
 function get_params(cfg::DayaBaySystematics)
-    params = ()
+    params = (;)
 end
 
 function get_priors(cfg::DayaBaySystematics)
-    priors = ()
+    priors = (;)
 end
 
-function get_daya_bay_flux(filename)
+function get_daya_bay_flux()
+    # read in spectrum
     spectrum = CSV.read(joinpath(@__DIR__, "daya_bay_flux.csv"), DataFrame, header=1, delim="  ");
     bin_edges = sort(union(spectrum[!, 1], spectrum[!, 2]))
     flux = spectrum[!, 3]
     flux_errs = spectrum[!, 4]
     binc = bin_edges[1:end-1] + diff(bin_edges) / 2
-    itp = Interpolator(binc, flux)
+    # read in correlation matrix
+    correlation = CSV.read("daya_bay_flux_correlation.csv", DataFrame, delim="  ", header=0)
+    correlation = Matrix(correlation[51:end, 51:end-1]);
+    # decompose into eigenvectors
+    decomp = eigen(correlation);
+    diag = Diagonal(decomp.values);
+    #convert eigenvalues (variances) to standard deviations
+    stds = sqrt.(decomp.values);
+    stacked_eigenvalues = reshape(repeat(stds, outer=stds.size), (25, 25));
+    pull_matrix = decomp.vectors .* transpose(stacked_eigenvalues)
+
+    # Create matrix
+    M = zeros(length(binc), length(binc))   # first index i, second index n
+    for (i, (l, h)) in enumerate(zip(bin_edges[1:end-1], bin_edges[2:end]))
+        for n in eachindex(binc)
+            func(E) = node_itp[n](E) * huber(E, (U_235=1,)) * x_sec_itp(E)
+            integrand(E, p) = func(E) > 0 ? func(E) : 0.
+            prob = IntegralProblem(integrand, (l, h))
+            sol = solve(prob, QuadGKJL())
+            M[i, n] = sol.u / (h - l)
+        end
+    end
+    M_inv = inv(M)
+    nodes = Diagonal(ones(25));
+    node_itp = [Interpolator(binc, nodes[i, :]) for i in eachindex(binc)];
+    y_0 = M_inv * flux
+    y_nk = M_inv * pull_matrix;
+    (;y_0, y_nk, node_itp)
 end
 
 function get_nominal_flux(cfg::DayaBayFlux)
-end
+    nominal_flux = get_daya_bay_flux()
+    y_0 = nominal_flux.y_0
+    y_nk = nominal_flux.y_nk
+    node_itp = nominal_flux.node_itp
     
-function get_nominal_flux(cfg::DayaBayFlux)
+    function nominal(E)
+        _huber = huber(E, cfg.isotope_Ratio)
+        phi_0 = sum([y_0[i] * node_itp[i](E) for i in eachindex(y_0)])
+        psi_k = sum([y_nk[n, :] * node_itp[n](E) for n in eachindex(y_0)])
+        return _huber * phi_0
+    end
+    nominal
+end
+
+function get_sys_flux(cfg::DayaBayFlux)
+    nominal_flux = get_daya_bay_flux()
+    y_0 = nominal_flux.y_0
+    y_nk = nominal_flux.y_nk
+    node_itp = nominal_flux.node_itp
+    
+    function systematic(E, pulls)
+        _huber = huber(E, cfg.isotope_Ratio)
+        psi_k = sum([y_nk[n, :] * node_itp[n](E) for n in eachindex(y_0)])
+        return _huber * sum(pulls .*psi_k)
+    end
+    systematic
 end
 
 end
