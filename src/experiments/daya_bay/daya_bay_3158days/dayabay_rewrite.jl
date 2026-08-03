027eac9a22b0951f2d7ba33f969c13cde7057d20
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2026-08-03 16:30:15 +0200

    change dayabay no-osc spectrum

---
+++ src/experiments/daya_bay/daya_bay_3158days/dayabay_rewrite.jl
@@ -0,0 +1,318 @@
+module dayabay_rewrite
+using DataFrames
+using CSV
+using LinearAlgebra
+using Distributions
+using DataStructures
+using CairoMakie
+using Accessors
+using Logging
+import ..Newtrinos
+
+@kwdef struct DayaBay <: Newtrinos.Experiment
+    physics::NamedTuple
+    params::NamedTuple
+    priors::NamedTuple
+    assets::NamedTuple
+    forward_model::Function
+    plot::Function
+end
+
+function default_physics()
+    osc = Newtrinos.osc.configure()
+    xsec = Newtrinos.ibd_xsec.configure()
+    flux = Newtrinos.reactor_flux.configure()
+    (; osc, xsec, flux)
+end
+
+function configure(physics=default_physics())
+    physics = (;physics.osc, physics.xsec, physics.flux)
+    assets = get_assets(physics)
+    return DayaBay(
+        physics = physics,
+        params = (;),
+        priors = (;),
+        assets = assets,
+        forward_model = get_forward_model(physics, assets),
+        plot = get_plot(physics, assets)
+    )
+end
+
+function get_assets(physics; datadir = @__DIR__)
+
+    @info "Loading dayabay data"
+    # Experimental Halls
+    EH_list = [1, 2, 3]
+    
+    # Data-taking Periods
+    period_list = ["Six", "Eight", "Seven"]
+    period_idx = [20, 49, 78]
+    periods_dict = Dict("Six" => 6, "Eight" => 8, "Seven" => 7)    # must...resist....
+    
+    # Reactors
+    reactor_list = ["D1", "D2", "L1", "L2", "L3", "L4"]
+    
+    # Correlation matrix from Fig. 29 in https://arxiv.org/pdf/1607.05378.pdf
+    corr_mat_df = CSV.read(joinpath(datadir, "DayaBay_CorrMat_arXiv_1607.05378.txt"), DataFrame, delim=' ')
+    corr_mat = Symmetric(Matrix(corr_mat_df[:, 5:end]))
+    
+    # Fractional systematic uncertainty on the expected IBD energy spectra
+    rel_unc_diag = corr_mat_df.diag_sys_unc_relative_to_spectrum;
+
+    # from https://arxiv.org/pdf/2211.14988
+    lifetime = Dict("Six-AD Period" => 217, "Eight-AD Period" => 1524, "Seven-AD Period" => 1417)
+    
+    # Reactors / Baselines, etc
+    exp_dict = Dict("Detector" => ["AD1", "AD2", "AD3", "AD8", "AD4", "AD5", "AD6", "AD7"],    # antinu-detectors
+                "EH" => ["EH1", "EH1", "EH2", "EH2", "EH3", "EH3", "EH3", "EH3"],   # exp hall
+                "Target [kg]" => [19941, 19967, 19891, 19944, 19917, 19989, 19892, 19931],   # to scale linearly expected events
+                "Efficiency" => [0.7743, 0.7716, 0.8127, 0.8105, 0.9513, 0.9514, 0.9512, 0.9513],  # to scale linearly expected events
+                "Six-AD Period" => [true, true, true, false, true, true, true, false],   # online time
+                "Eight-AD Period" => [true, true, true, true, true, true, true, true],
+                "Seven-AD Period" => [true, false, true, true, true, true, true, true],
+                "D1" => [362.38, 357.94, 1332.48, 1337.43, 1919.63, 1917.52, 1925.26, 1923.15],   # baselines to all reactors
+                "D2" => [371.76, 368.41, 1358.15, 1362.88, 1894.34, 1891.98, 1899.86, 1897.51],   # D: Daya Bay
+                "L1" => [903.47, 903.35, 467.57, 472.97, 1533.18, 1534.92, 1538.93, 1540.67],     # L: the other one...
+                "L2" => [817.16, 816.90, 489.58, 495.35, 1533.63, 1535.03, 1539.47, 1540.87],
+                "L3" => [1353.62, 1354.23, 557.58, 558.71, 1551.38, 1554.77, 1556.34, 1559.72],
+                "L4" => [1265.32, 1265.89, 499.21, 501.07, 1524.94, 1528.05, 1530.08, 1533.18])
+    df_exp = DataFrame(exp_dict);
+    
+    function parse(fname, idx, len)
+        # Parsing the DayaBay format CSV files
+        header = Array(CSV.read(fname, DataFrame, delim=' ', skipto=idx-1, limit=1, ignorerepeated=true, header=false));
+        header = header[2:end];
+        df = CSV.read(fname, DataFrame, delim=' ', skipto=idx, limit=len, ignorerepeated=true, header=false);
+        rename!(df, header, makeunique=false);
+        return df
+    end
+    
+    # Dicts to fill
+    dfBKG_dict = Dict()
+    dfIBD_dict = Dict()
+    
+    for EH in EH_list
+        fileBKG = joinpath(datadir,  "DayaBay_BackgroundSpectrum_EH$(EH)_3158days.txt")
+        fileIBD = joinpath(datadir,  "DayaBay_IBDPromptSpectrum_EH$(EH)_3158days.txt")
+        dfIBD_dict["dfIBD_EH$EH"] = parse(fileIBD, 11, size(corr_mat_df, 1))
+        for i in 1:length(period_list)
+            period = period_list[i]
+            idx = period_idx[i]
+            dfBKG_dict["dfBKG_$(period)_EH$(EH)"] = parse(fileBKG, idx, size(corr_mat_df, 1))
+        end
+        # Sum over data-taking periods
+        dfIBD_dict["dfIBD_EH$EH"][!, "Nobs"] = sum(eachcol(dfIBD_dict["dfIBD_EH$EH"][!, ["Nobs_6AD", "Nobs_8AD", "Nobs_7AD"]]))
+        dfIBD_dict["dfIBD_EH$EH"][!, "Npred"] = sum(eachcol(dfIBD_dict["dfIBD_EH$EH"][!, ["Npred_6AD", "Npred_8AD", "Npred_7AD"]]))
+        BKG = zeros(size(dfIBD_dict["dfIBD_EH$EH"], 1))
+        for period in period_list
+            BKG .+= dfBKG_dict["dfBKG_$(period)_EH$(EH)"].Nbkg
+        end
+        dfIBD_dict["dfIBD_EH$EH"][!, "BKG"] = BKG
+        dfIBD_dict["dfIBD_EH$EH"][!, "N"] = dfIBD_dict["dfIBD_EH$EH"].Nobs .- BKG
+    end
+
+    energy_bins = copy(dfBKG_dict["dfBKG_Six_EH3"].Emin)
+    energy = copy(dfBKG_dict["dfBKG_Six_EH3"].Ec)
+    push!(energy_bins, dfBKG_dict["dfBKG_Six_EH3"].Emax[end])
+    
+    # Setup for DayaBay bestfit to recalculate unoscillated spectrum
+    bestift_osc = Newtrinos.osc.configure()
+    @reset bestift_osc.params.θ₁₂ = asin(sqrt(0.307))
+    @reset bestift_osc.params.θ₁₃ = asin(sqrt(0.0851)) * 0.5
+    @reset bestift_osc.params.θ₂₃ = asin(sqrt(0.57))
+    @reset bestift_osc.params.δCP = 0.
+    @reset bestift_osc.params.Δm²₂₁ = 7.53e-5
+    @reset bestift_osc.params.Δm²₃₁ = 2.466e-3 + bestift_osc.params[:Δm²₂₁]
+    
+    ad_contribs_to_far_hall = Float64[]
+    E_arrs = Vector{Vector{Float64}}()
+    L_arrs = Vector{Vector{Float64}}()
+    Npred_EH3_nooscs = Vector{Vector{Float64}}()
+
+    #maps anti nu energy to prompt energy
+    res = CSV.read("response_matrix.txt", DataFrame, delim="\t", skipto=6)
+    res = Matrix(res[!, 1:end-1])
+    res = float.(res);
+
+    #normalise s.t. we can use this as a proper smearing matrix
+    for (c, v) in enumerate(eachrow(res))
+        # println(c, v)
+        # avoid dividing by zero
+        if any(isnan, v) || iszero(v)
+            res[c, :] .= zeros(size(v))
+        else
+            res[c, :] .= res[c, :] ./ sum(v)
+        end
+    end
+
+    reso = transpose(res)
+
+    #bin edges
+    E_antinu = Vector(range(1, 13; step=0.01))
+    E_prompt = Vector(range(1-0.05, 8; step=0.05))
+    E_prompt[1] = 0.7
+
+    # bin centers
+    E_antinu_binc = (E_antinu[1:end-1] + E_antinu[2:end]) / 2;
+    E_prompt_binc = (E_prompt[1:end-1] + E_prompt[2:end]) / 2;
+
+    # flux and ibd cross section
+    flux = physics.flux.nominal_flux
+    xsec = physics.xsec.xsec
+
+    flux_eval = flux.(E_antinu_binc)
+    xsec_eval = xsec.(E_antinu_binc)
+    ibd_weighted_flux = flux_eval .* xsec_eval
+
+    predicted_no_oscs = Vector{Vector{Float64}}()
+    flux_weights = Vector{Vector{Float64}}()
+    
+    for period in period_list
+    
+        df_period = filter(row -> row["$(period)-AD Period"], df_exp)
+        # takes only data of EH3 (furthest)
+        df_period = filter(row -> row["EH"] == "EH3", df_period)
+    
+        E_arr = dfIBD_dict["dfIBD_EH1"].Ec .+ 0.78
+        # indices: AD x reactor
+        L_matrix = df_period[:, ["D1", "D2", "L1", "L2", "L3", "L4"]]
+        # flatten
+        L_arr = vec(Matrix(L_matrix))
+    
+        # for comparison with proper prediction, keep this
+        Npred_EH3_after_best_fit_osc = dfIBD_dict["dfIBD_EH3"][:, "Npred_$(periods_dict[period])AD"]
+        best_fit_prob_arr = bestift_osc.osc_prob(E_arr, L_arr, bestift_osc.params, anti=true)[:, :, 1, 1]'
+        # get the survival probability as flux scale, scale flux by respective baseline squared, normalise by inverse squared baselines
+        baseline_average_best_fit_prob_arr = vec(sum(best_fit_prob_arr ./ (L_arr .^ 2), dims=1) ./ sum(1 ./(L_arr .^ 2)))
+        # unoscillated N predicted EH3:
+        Npred_EH3_noosc = Npred_EH3_after_best_fit_osc ./ baseline_average_best_fit_prob_arr
+        # TODO: ONLY REPLACE THIS HERE! DO NOT CHANGE ANYTHING ELSE
+        # tis but the flux / (4*pi*L^2) * IBD, put through the detector response, multiplied by respective efficiency and target mass
+    
+        # Npred_EH3_noosc_from_flux = 
+        push!(E_arrs, E_arr)
+        push!(L_arrs, L_arr)
+        push!(Npred_EH3_nooscs, Npred_EH3_noosc)
+        
+        eff = df_period[:, "Efficiency"]
+        mass = df_period[:, "Target [kg]"]
+        efficiency = repeat(eff, length(reactor_list))
+        target_mass = repeat(mass, length(reactor_list));
+
+        # later, include P_ee here as well
+        # now, this has dimension (AD x reactor).flatten(), then will have additional energy dimension to account for oscillations
+        flux_weight = lifetime["$period-AD Period"] * efficiency .* target_mass ./ L_arr.^2 / sum(1 ./ L_arr.^2)
+        smeared = reso * ibd_weighted_flux .* sum(flux_weight)
+        idx = searchsortedlast.(Ref(energy_bins), E_prompt_binc)
+        result = zeros(size(energy_bins[1:end-1]))
+        # integrate by summing over Eprompt in the bins of Dayabay
+        for c in eachindex(energy_bins[1:end-1])
+            result[c] = sum(smeared[idx .== c])
+        end
+
+        push!(predicted_no_oscs, result)
+        push!(flux_weights, flux_weight)
+        for reactor in reactor_list
+            # remaining index here (:) is AD
+            ad_contrib_this_period_this_reactor = (1 ./ df_period[:, reactor] .^ 2) .* df_period[:, "Target [kg]"] .* df_period[:, "Efficiency"]
+            append!(ad_contribs_to_far_hall, ad_contrib_this_period_this_reactor)
+        end
+    end
+    
+    normalized_ad_contribs_to_far_hall = ad_contribs_to_far_hall ./ sum(ad_contribs_to_far_hall)
+    covmat_prefactor = sum(normalized_ad_contribs_to_far_hall .^ 2)
+
+    observed = convert(Vector{Float64}, dfIBD_dict["dfIBD_EH3"].N)
+
+
+    
+    assets = (;
+        E_arrs, 
+        L_arrs, 
+        Npred_EH3_nooscs,
+        rel_unc_diag,
+        corr_mat,
+        covmat_prefactor,
+        observed,
+        period_list,
+        energy_bins,
+        energy,
+        predicted_no_oscs,
+        flux_weights,
+        #ibd_weighted_flux
+        #xsec_eval,
+        flux_eval,
+    )
+
+end
+
+
+function get_expected_per_period(params, period, physics, assets)
+    E = assets.E_arrs[period]
+    L = assets.L_arrs[period]
+    prob_arr = physics.osc.osc_prob(E, L, params, anti=true)[:, :, 1, 1]'
+    L2 = L .^ 2
+    prob = vec(sum(prob_arr./L2, dims=1) ./ sum(1 ./L2))
+    # shift all scales and stuff in Npred_EH3_nooscs, i.e. this becomes part of the model,
+    # the modification for the oscillation can stay where it is!
+    Npred_EH3_with_osc = assets.Npred_EH3_nooscs[period] .* prob
+end
+
+# Define function to give the expected events at the far hall (EH3)
+function get_expected(params, physics, assets)
+    sum([get_expected_per_period(params, period, physics, assets) for period in 1:length(assets.period_list)])
+end
+
+function get_forward_model(physics, assets)
+    function forward_model(params)
+        exp_events = get_expected(params, physics, assets)
+        cov = Symmetric(Diagonal(exp_events .* assets.rel_unc_diag) * assets.corr_mat * Diagonal(exp_events .* assets.rel_unc_diag)) * assets.covmat_prefactor + Diagonal(exp_events)
+        Distributions.MvNormal(exp_events, cov)
+    end
+end
+
+function get_plot(physics, assets)
+
+    function plot(params, data=assets.observed)
+        
+        m = mean(get_forward_model(physics, assets)(params))
+        v = var(get_forward_model(physics, assets)(params))
+    
+        f = Figure()
+        ax = Axis(f[1,1])
+        
+        plot!(ax, assets.energy, data, color=:black, label="Observed")
+        stephist!(ax, assets.energy, weights=m, bins=assets.energy_bins, label="Expected")
+        barplot!(ax, assets.energy, m .+ sqrt.(v), width=diff(assets.energy_bins), gap=0, fillto= m .- sqrt.(v), alpha=0.5, label="Standard Deviation")
+        
+        ax.ylabel="Counts"
+        ax.title="Daya Bay"
+        axislegend(ax, framevisible = false)
+        
+        
+        ax2 = Axis(f[2,1])
+        plot!(ax2, assets.energy, data ./ m, color=:black, label="Observed")
+        hlines!(ax2, 1, label="Expected")
+        barplot!(ax2, assets.energy, 1 .+ sqrt.(v) ./ m, width=diff(assets.energy_bins), gap=0, fillto= 1 .- sqrt.(v)./m, alpha=0.5, label="Standard Deviation")
+        ylims!(ax2, 0.9, 1.1)
+        
+        ax.xticksvisible = false
+        ax.xticklabelsvisible = false
+        
+        rowsize!(f.layout, 1, Relative(3/4))
+        rowgap!(f.layout, 1, 0)
+        
+        ax2.xlabel="Eₚ (MeV)"
+        ax2.ylabel="Counts/Expected"
+    
+        xlims!(ax, minimum(assets.energy_bins), maximum(assets.energy_bins))
+        xlims!(ax2, minimum(assets.energy_bins), maximum(assets.energy_bins))
+        
+        ylims!(ax, 0, 60000)
+        
+        f
+    
+    end
+end
+
+end
--- src/physics/reactor_flux.jl
+++ src/physics/reactor_flux.jl
@@ -3,11 +3,13 @@ module reactor_flux
 using Interpolations
 using DataStructures
 using PCHIPInterpolation
+using LinearAlgebra
+using Integrals
 using CSV
 using DataFrames
 using ..Newtrinos
 
-export ReactorFluxConfig, HuberFlux
+export ReactorFluxConfig, HuberFlux, HuberSystematics, DayaBayFlux, DayaBaySystematics
 
 const datadir = @__DIR__
 
@@ -18,10 +20,11 @@ abstract type NominalFluxModel end
 abstract type FluxSystematicsModel end
 
 
-@kwdef struct ReactorFluxConfig{F<:NominalFluxModel, S<:FluxSystematicsModel}
+# TODO: add name to flux config to identify reactors
+@kwdef struct ReactorFluxConfig{F<:NominalFluxModel, S<:FluxSystematicsModel}#, X:<Newtrinos.ibd_xsec.IBDXsec}
+    nominal_model::F = DayaBayFlux()
+    systematics_model::S = DayaBaySystematics()
     isotope_ratio::NamedTuple = (U_235 = 0.61, Pu_239 = 0.33, Pu_241 = 0.06,)
-    nominal_model::F = HuberFlux(isotope_ratio)
-    systematics_model::S = HuberSystematics()
 end
 
 
@@ -34,13 +37,11 @@ end
 
 
 struct DayaBayFlux <: NominalFluxModel
-    cfg::ReactorFluxConfig
 end
 
 
 struct DayaBaySystematics <: FluxSystematicsModel
 end
-
 
 
 @kwdef struct ReactorFlux <: Newtrinos.Physics
@@ -50,6 +51,7 @@ end
     nominal_flux::Function
     sys_flux::Function
 end
+
 
 function configure(cfg::ReactorFluxConfig = ReactorFluxConfig())
     ReactorFlux(
@@ -66,9 +68,11 @@ function get_params(cfg::HuberFlux)
     return (;)
 end
 
+
 function get_priors(cfg::HuberFlux)
     return (;)
 end
+
 
 function get_nominal_flux(cfg::HuberFlux)
     isotope_ratio = cfg.isotope_ratio
@@ -84,45 +88,37 @@ function get_nominal_flux(cfg::HuberFlux)
             return exp(exponent)
         end
         isotope_weighted_flux = 0.
-        for (iso, weight) in pairs(fraction)
+        for (iso, weight) in pairs(isotope_ratio)
             isotope_weighted_flux += weight * single_flux(E, iso)
         end
         return isotope_weighted_flux
 
     end
-
     nominal_flux
 end
 
-function get_sys_flux(cfg::HuberFlux)
+
+function get_sys_flux(cfg::HuberSystematics)
     func(x) = 0.
 end
+
 
 function get_priors(cfg::HuberFlux)
     return (;)
 end
 
+
 function get_params(cfg::HuberFlux)
     return (;)
 end
 
-"""
-function configure(cfg::ReactorFluxConfig=ReactorFluxConfig())
-    ReactorFlux(
-        cfg=cfg,
-        params = get_params(cfg.systematics_model),
-        priors = get_priors(cfg.systematics_model),
-        nominal_flux = get_nominal_flux(cfg.nominal_model),
-        sys_flux = get_sys_flux(cfg.systematics_model)
-        )
-end
-"""
 
-function get_params(cfg::DayaBaySystematics)
+
+function get_params(cfg::DayaBayFlux)
     params = (;)
 end
 
-function get_priors(cfg::DayaBaySystematics)
+function get_priors(cfg::DayaBayFlux)
     priors = (;)
 end
 
@@ -134,30 +130,52 @@ function get_daya_bay_flux()
     flux_errs = spectrum[!, 4]
     binc = bin_edges[1:end-1] + diff(bin_edges) / 2
     # read in correlation matrix
-    correlation = CSV.read("daya_bay_flux_correlation.csv", DataFrame, delim="  ", header=0)
-    correlation = Matrix(correlation[51:end, 51:end-1]);
+    correlation = CSV.read(joinpath(@__DIR__, "daya_bay_flux_correlation.csv"), DataFrame, delim="  ", header=0)
+    correlation = Matrix(correlation[51:end, 51:end-1])
     # decompose into eigenvectors
-    decomp = eigen(correlation);
-    diag = Diagonal(decomp.values);
+    decomp = eigen(correlation)
+    diag = Diagonal(decomp.values)
     #convert eigenvalues (variances) to standard deviations
-    stds = sqrt.(decomp.values);
-    stacked_eigenvalues = reshape(repeat(stds, outer=stds.size), (25, 25));
+    stds = sqrt.(decomp.values)
+    stacked_eigenvalues = reshape(repeat(stds, outer=stds.size), (25, 25))
     pull_matrix = decomp.vectors .* transpose(stacked_eigenvalues)
+    nodes = Diagonal(ones(25))
+    node_itp = []
+    for i in eachindex(binc)
+        interp = Interpolator(binc, nodes[i, :])
+        func(E) = interp(E) > 0 ? interp(E) : 0.
+        push!(node_itp, func)
+    end
+    
+    isotope_ratio = (U_235 = 0.61, Pu_239 = 0.33, Pu_241 = 0.06,)
+    huber_config = ReactorFluxConfig(nominal_model=HuberFlux(isotope_ratio), systematics_model=HuberSystematics(), isotope_ratio=isotope_ratio)
+    huber_flux = configure(huber_config)
+    huber = huber_flux.nominal_flux
+    
+    xsec = Newtrinos.ibd_xsec.configure()
+    x_sec_itp = xsec.xsec
+
+    x_sec_integral = []
+    #integrate xsec over energy bins
+    for (i, (l, h)) in enumerate(zip(bin_edges[1:end-1], bin_edges[2:end]))
+        integrand(E, p) = x_sec_itp(E)
+        prob = IntegralProblem(integrand, (l, h))
+        sol = solve(prob, QuadGKJL())
+        push!(x_sec_integral, sol.u)
+    end
 
     # Create matrix
     M = zeros(length(binc), length(binc))   # first index i, second index n
     for (i, (l, h)) in enumerate(zip(bin_edges[1:end-1], bin_edges[2:end]))
         for n in eachindex(binc)
-            func(E) = node_itp[n](E) * huber(E, (U_235=1,)) * x_sec_itp(E)
-            integrand(E, p) = func(E) > 0 ? func(E) : 0.
+            func(E) = node_itp[n](E) * huber(E) * x_sec_itp(E)
+            integrand(E, p) = func(E) > 0. ? func(E) : 0.
             prob = IntegralProblem(integrand, (l, h))
             sol = solve(prob, QuadGKJL())
             M[i, n] = sol.u / (h - l)
         end
     end
     M_inv = inv(M)
-    nodes = Diagonal(ones(25));
-    node_itp = [Interpolator(binc, nodes[i, :]) for i in eachindex(binc)];
     y_0 = M_inv * flux
     y_nk = M_inv * pull_matrix;
     (;y_0, y_nk, node_itp)
@@ -168,9 +186,12 @@ function get_nominal_flux(cfg::DayaBayFlux)
     y_0 = nominal_flux.y_0
     y_nk = nominal_flux.y_nk
     node_itp = nominal_flux.node_itp
-    
+    isotope_ratio = (U_235 = 0.61, Pu_239 = 0.33, Pu_241 = 0.06,)
+    huber_config = ReactorFluxConfig(nominal_model=HuberFlux(isotope_ratio), systematics_model=HuberSystematics(), isotope_ratio=isotope_ratio)
+    flux = configure(huber_config)
+    huber = flux.nominal_flux
     function nominal(E)
-        _huber = huber(E, cfg.isotope_Ratio)
+        _huber = huber(E)
         phi_0 = sum([y_0[i] * node_itp[i](E) for i in eachindex(y_0)])
         psi_k = sum([y_nk[n, :] * node_itp[n](E) for n in eachindex(y_0)])
         return _huber * phi_0
@@ -178,14 +199,18 @@ function get_nominal_flux(cfg::DayaBayFlux)
     nominal
 end
 
-function get_sys_flux(cfg::DayaBayFlux)
+function get_sys_flux(cfg::DayaBaySystematics)
     nominal_flux = get_daya_bay_flux()
     y_0 = nominal_flux.y_0
     y_nk = nominal_flux.y_nk
     node_itp = nominal_flux.node_itp
+    isotope_ratio = (U_235 = 0.61, Pu_239 = 0.33, Pu_241 = 0.06,)
+    huber_config = ReactorFluxConfig(nominal_model=HuberFlux(isotope_ratio), systematics_model=HuberSystematics(), isotope_ratio=isotope_ratio)
+    flux = configure(huber_config)
+    huber = flux.nominal_flux
     
     function systematic(E, pulls)
-        _huber = huber(E, cfg.isotope_Ratio)
+        _huber = huber(E)
         psi_k = sum([y_nk[n, :] * node_itp[n](E) for n in eachindex(y_0)])
         return _huber * sum(pulls .*psi_k)
     end
